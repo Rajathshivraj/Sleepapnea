1d06fef15f7baa34d05c7f3559d07cb97486ab8e
Rajathshivraj <rajathdsu@gmail.com>
2025-10-06 03:34:26 +0530

    working

---
+++ sleep-ai-insight-main/src/App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import Home from "./pages/Home";
+import Research from "./pages/Research";
+import Literature from "./pages/Literature";
+import Datasets from "./pages/Datasets";
+import Code from "./pages/Code";
+import Publications from "./pages/Publications";
+import Team from "./pages/Team";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/research" element={<Research />} />
+            <Route path="/literature" element={<Literature />} />
+            <Route path="/datasets" element={<Datasets />} />
+            <Route path="/code" element={<Code />} />
+            <Route path="/publications" element={<Publications />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
